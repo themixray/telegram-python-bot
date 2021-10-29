76c6835c36c459417f024bb3df6b5f43d2e7455b
themixray <35273590+themixray@users.noreply.github.com>
2021-10-29 23:43:35 +0300

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,20 +7,23 @@ class client:
     def __init__(self, token):
         self.updater = telegram.ext.Updater(token)
         self.whitelist = None
-    def command(self, func):
-        def fwwh(func, whitelist, update, context):
-            if whitelist == None or update.message.from_user.id in whitelist:
-                func(update, context)
-        func = lambda update,context:fwwh(func,self.whitelist,update,context)
-        self.updater.dispatcher.add_handler(
-            telegram.ext.CommandHandler(func.__name__,func))
+    def command(self, name=None):
+        def real_decorator(func):
+            nonlocal name
+            def fwwh2(update,context):
+                if self.whitelist == None or update.message.from_user.id in self.whitelist:
+                    func(update, context)
+            if name == None:
+                name = func.__name__
+            self.updater.dispatcher.add_handler(
+                telegram.ext.CommandHandler(name,fwwh2))
+        return real_decorator
     def message(self, func):
-        def fwwh(func, whitelist, update, context):
-            if whitelist == None or update.message.from_user.id in whitelist:
+        def fwwh2(update,context):
+            if self.whitelist == None or update.message.from_user.id in self.whitelist:
                 func(update, context)
-        func = lambda update,context:fwwh(func,self.whitelist,update,context)
         self.updater.dispatcher.add_handler(
-            telegram.ext.MessageHandler(telegram.ext.Filters.text,func))
+            telegram.ext.MessageHandler(telegram.ext.Filters.text,fwwh2))
     def start(self):
         self.updater.start_polling()
         self.updater.idle()
