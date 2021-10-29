309f638a824b96aaed4bc2fb32f63492c89e1d9c
themixray <35273590+themixray@users.noreply.github.com>
2021-10-29 23:20:35 +0300

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,26 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import telegram
+import telegram.ext
 import requests
 import re
 
 class client:
     def __init__(self, token):
-        self.updater = Updater(token)
+        self.updater = telegram.ext.Updater(token)
+        self.whitelist = None
     def command(self, func):
+        def fwwh(func, whitelist, update, context):
+            if whitelist == None or update.message.from_user.id in whitelist:
+                func(update, context)
+        func = lambda update,context:fwwh(func,self.whitelist,update,context)
         self.updater.dispatcher.add_handler(
-            CommandHandler(func.__name__,func))
+            telegram.ext.CommandHandler(func.__name__,func))
+    def message(self, func):
+        def fwwh(func, whitelist, update, context):
+            if whitelist == None or update.message.from_user.id in whitelist:
+                func(update, context)
+        func = lambda update,context:fwwh(func,self.whitelist,update,context)
+        self.updater.dispatcher.add_handler(
+            telegram.ext.MessageHandler(telegram.ext.Filters.text,func))
     def start(self):
         self.updater.start_polling()
         self.updater.idle()
