3ce065613a9fea2c3d43538634346acc1451adf0
themixray <35273590+themixray@users.noreply.github.com>
2021-10-30 20:33:19 +0300

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,23 +5,27 @@ import re
 
 class client:
     def __init__(self, token):
+        self.bot = telegram.Bot(token)
         self.updater = telegram.ext.Updater(token)
         self.whitelist = None
-    def command(self, name=None):
+        self.commands = []
+    def command(self, name=None, description=''):
         def real_decorator(func):
-            nonlocal name
-            def fwwh2(update,context):
-                if self.whitelist == None or update.message.from_user.id in self.whitelist:
-                    func(update, context)
+            nonlocal name, self
             if name == None:
                 name = func.__name__
+            self.commands.append(telegram.BotCommand(name,description))
+            def fwwh2(update, context):
+                if self.whitelist == None or update.message.from_user.id in self.whitelist:
+                    args = update.message.text[len(name)+2:]
+                    func(update, args)
             self.updater.dispatcher.add_handler(
                 telegram.ext.CommandHandler(name,fwwh2))
         return real_decorator
     def message(self, func):
         def fwwh2(update,context):
             if self.whitelist == None or update.message.from_user.id in self.whitelist:
-                func(update, context)
+                func(update)
         self.updater.dispatcher.add_handler(
             telegram.ext.MessageHandler(telegram.ext.Filters.text,fwwh2))
     def start(self):
